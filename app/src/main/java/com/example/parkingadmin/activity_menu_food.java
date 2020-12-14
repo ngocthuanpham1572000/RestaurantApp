8527c9ee6506dff1b58fc890f9aebe77c88bfa44
ngocthuanpham1572000 gmail.com <ngocthuanpham1572000@gmail.com>
2020-12-14 10:30:51 +0700

    Add

---
+++ app/src/main/java/com/example/parkingadmin/activity_menu_food.java
@@ -0,0 +1,14 @@
+package com.example.parkingadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_menu_food extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_food);
+    }
+}
